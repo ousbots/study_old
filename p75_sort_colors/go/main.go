34a60b3ad0a830a9d239ae20c14dfd93ff0c7c4b
Tim Ousborne <dev@sourbone.net>
2022-05-28 00:02:58 -0600

    p75 sort colors (go): improve performance

--- p75_sort_colors/go/main.go
+++ p75_sort_colors/go/main.go
@@ -1,27 +1,27 @@
 package p75_sort_colors
 
 func sortColors(nums []int) {
-	red := 0
 	white := 0
+	blue := 0
+	index := 0
 
 	for _, num := range nums {
 		switch num {
 		case 0:
-			red++
+			nums[index] = 0
+			index++
 		case 1:
 			white++
+		case 2:
+			blue++
 		}
 	}
 
-	for i := range nums {
-		if i < red+white {
-			if i < red {
-				nums[i] = 0
-			} else {
-				nums[i] = 1
-			}
-		} else {
-			nums[i] = 2
-		}
+	for i := index; i < index+white; i++ {
+		nums[i] = 1
+	}
+
+	for i := index + white; i < len(nums); i++ {
+		nums[i] = 2
 	}
 }
